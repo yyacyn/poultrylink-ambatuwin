aee1a8c59284e0ce275fdf0b4f79d88fe1cf18e9
Anargya <rbannaslam@apps.ipb.ac.id>
2024-11-07 11:21:54 +0700

    tambahan di index

--- js/main.js
+++ js/main.js
@@ -126,10 +126,10 @@
         $('.back-to-top').fadeOut('slow');
     }
     });
-    $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
-        return false;
-    });
+    // $('.back-to-top').click(function () {
+    //     $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+    //     return false;
+    // });
 
 
 })(jQuery);
